c823413d2dacdfb3ef827267111fe14039fd0393
azxca1731 <azxca1731@gmail.com>
2019-01-01 11:25:26 +0900

    remove example

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,8 @@ export default () => ({
           {
             loader: "babel-loader",
             options: {
-              presets: ["@babel/preset-env", "@babel/preset-react"]
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+              plugins: ["@babel/plugin-proposal-class-properties"]
             }
           }
         ]
