b798f4110e98475a2ba1147e9d0b4f1bb5d250c2
azxca1731 <azxca1731@gmail.com>
2019-01-27 17:02:01 +0900

    addon 추가 설정

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,11 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator, addParameters } from "@storybook/react";
+import { withKnobs } from "@storybook/addon-knobs";
+import { withBackgrounds } from "@storybook/addon-backgrounds";
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/);
+addDecorator(withBackgrounds([{ name: "white", value: "#ffffff", default: true }, { name: "grey", value: "#C0C0C0" }, { name: "black", value: "#000000" }]));
+addDecorator(withKnobs);
+
+const req = require.context("../stories", true, /.stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
