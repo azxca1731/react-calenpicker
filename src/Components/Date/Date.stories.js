f51aacf9bd73358c5d1db1434c7fecde7c1a22a1
azxca1731 <azxca1731@gmail.com>
2019-01-28 01:36:57 +0900

    데이트 스토리 재설정

--- src/Components/Date/Date.stories.js
+++ src/Components/Date/Date.stories.js
@@ -1,10 +1,18 @@
 import React from "react";
-
+import { ThemeProvider } from "styled-components";
 import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
 
+import light from "Styles/theme/light";
+import dark from "Styles/theme/dark";
 import Date from "Components/Date";
 
 storiesOf("Components", module)
   .addDecorator(withInfo)
-  .add("Date", () => <Date />);
+  .addDecorator(story => <ThemeProvider theme={light}>{story()}</ThemeProvider>)
+  .add("Date light", () => <Date />);
+
+storiesOf("Components", module)
+  .addDecorator(withInfo)
+  .addDecorator(story => <ThemeProvider theme={dark}>{story()}</ThemeProvider>)
+  .add("Date Dark", () => <Date />);
