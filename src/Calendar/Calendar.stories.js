d17f9d5402ba98b56e080c23bb607c0e27245136
azxca1731 <azxca1731@gmail.com>
2019-01-28 00:33:31 +0900

    캘린더 옵션별로 정리

--- src/Calendar/Calendar.stories.js
+++ src/Calendar/Calendar.stories.js
@@ -4,4 +4,10 @@ import { storiesOf } from "@storybook/react";
 
 import Calendar from "./Calendar";
 
-storiesOf("Calendar", module).add("캘린더 기본형", () => <Calendar />);
+storiesOf("Calendar", module)
+  .add("캘린더 기본형", () => <Calendar />)
+  .add("캘린더 테마 검은색", () => <Calendar theme="dark" />)
+  .add("캘린더 이번달만 보여주기 옵션", () => <Calendar onlyThisMonth />)
+  .add("텍스트 커스터마이징", () => <Calendar objectSetText={[{ text: "연말", date: "2018-12-31" }, { text: "휴가", date: "2018-12-26", isHoliday: true }]} />)
+  .add("캘린더 중복선택가능", () => <Calendar multiSelect />)
+  .add("캘린더 중복 듀플리케이트", () => <Calendar duplicate />);
