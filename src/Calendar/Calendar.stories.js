bb4c8a78e4746537951bba2f4dbf8f40d80400fb
azxca1731 <azxca1731@gmail.com>
2019-01-27 18:14:04 +0900

    기타 수정

---
+++ src/Calendar/Calendar.stories.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+
+import Calendar from "./Calendar";
+
+storiesOf("Calendar", module).add("캘린더 기본형", () => <Calendar />);
--- src/Components/Week/Week.stories.js
+++ src/Components/Week/Week.stories.js
@@ -4,4 +4,4 @@ import { storiesOf } from "@storybook/react";
 
 import Week from "Components/Week";
 
-storiesOf("Week", module).add("Week 기본형", () => <Week />);
+// storiesOf("Week", module).add("Week 기본형", () => <Week />);
