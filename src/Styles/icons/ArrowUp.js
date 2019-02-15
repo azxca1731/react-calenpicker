3ab662c3a0f6d498b24cb4763a9778a15761214b
azxca1731 <azxca1731@gmail.com>
2019-02-15 22:56:13 +0900

    이름 변경 및 디폴트 뷰박스 수정

--- src/Styles/icons/ArrowUp.js
+++ src/Styles/icons/ArrowUp.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const SVG = ({ style = {}, fill = "#000", width = "100%", viewBox = "0 0 12 9" }) => (
+const SVG = ({ style = {}, fill = "#000", width = "100%", viewBox = "0 0 1792 1792" }) => (
   <svg width={width} style={style} height={width} viewBox={viewBox} xmlns="http://www.w3.org/2000/svg">
     <path fill={fill} d="M1395 1184q0 13-10 23l-50 50q-10 10-23 10t-23-10l-393-393-393 393q-10 10-23 10t-23-10l-50-50q-10-10-10-23t10-23l466-466q10-10 23-10t23 10l466 466q10 10 10 23z" />
   </svg>
