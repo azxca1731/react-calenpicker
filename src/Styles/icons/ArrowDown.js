c41a03744d6d5ae55a6ae01673b015ed78d6b4a5
azxca1731 <azxca1731@gmail.com>
2019-02-16 15:27:11 +0900

    아이콘 클릭 가능하게 수정

--- src/Styles/icons/ArrowDown.js
+++ src/Styles/icons/ArrowDown.js
@@ -7,16 +7,19 @@ const Icon = styled.svg`
   cursor: pointer;
 `;
 
-const SVG = ({ style = {}, width = "15px", viewBox = "0 0 1792 1792" }) => (
-  <Icon width={width} style={style} height={width} viewBox={viewBox} xmlns="http://www.w3.org/2000/svg">
-    <path d="M1395 736q0 13-10 23l-466 466q-10 10-23 10t-23-10l-466-466q-10-10-10-23t10-23l50-50q10-10 23-10t23 10l393 393 393-393q10-10 23-10t23 10l50 50q10 10 10 23z" />
-  </Icon>
+const SVG = ({ style = {}, width = "15px", viewBox = "0 0 1792 1792", onClick = () => {} }) => (
+  <div onClick={onClick}>
+    <Icon width={width} style={style} height={width} viewBox={viewBox} xmlns="http://www.w3.org/2000/svg">
+      <path d="M1395 736q0 13-10 23l-466 466q-10 10-23 10t-23-10l-466-466q-10-10-10-23t10-23l50-50q10-10 23-10t23 10l393 393 393-393q10-10 23-10t23 10l50 50q10 10 10 23z" />
+    </Icon>
+  </div>
 );
 
 SVG.propTypes = {
   style: PropTypes.object,
   width: PropTypes.string,
-  viewBox: PropTypes.string
+  viewBox: PropTypes.string,
+  onClick: PropTypes.func
 };
 
 export default SVG;
