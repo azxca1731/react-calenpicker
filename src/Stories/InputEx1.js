3df54d6d19983ce972d24e75ea885f952426a7d9
DongDongKim <cjdtkddkrrl@daum.net>
2019-02-16 18:15:27 +0900

    InputEx1

--- src/Stories/InputEx1.js
+++ src/Stories/InputEx1.js
@@ -1,16 +1,52 @@
 import React, { Component } from "react";
 import Calendar from "../Calendar";
-// import styled from "styled-components";
+import styled from "styled-components";
 
-// const MonthDiv = styled.div`
-//   color: ${props => props.theme.fontColor};
-//   font-size: 150%;
-//   background-color:0 ${props => props.theme.backgroundColor};
-// `;
+const InputEx1Div1 = styled.div`
+  padding-top: 16px;
+  padding-bottom: 14px;
+  background: #ffffff;
+  display: table-cell;
+  border: 1px solid #ebebeb;
+  -webkit-transition: width 250ms;
+  -moz-transition: width 250ms;
+  transition: width 250ms;
+  vertical-align: middle;
+`;
 
-// const Month = props => {
-//   const { cssObject, month } = props;
-//   return <MonthDiv style={cssObject}>{month}</MonthDiv>;
+const InputEx1Div2 = styled.div`
+  display: table;
+  table-layout: fixed;
+  height: 100%;
+  width: 100%;
+`;
+
+const InputEx1Date = styled.div`
+  font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif;
+  font-size: 18px;
+  font-weight: 600;
+  color: rgb(72, 72, 72);
+`;
+
+const InputEx1BtnContent = styled.div`
+  overflow-wrap: break-word;
+  font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif;
+  font-size: 18px;
+  font-weight: 600;
+  line-height: 1.44444em;
+  color: rgb(72, 72, 72);
+  margin: 0px;
+`;
+
+const InputEx1Btn = styled.button`
+  background: none;
+  border: none;
+  padding: 0px;
+  overflow: hidden;
+  text-align: left;
+  white-space: nowrap;
+  width: 100%;
+`;
 
 class InputEx1 extends Component {
   constructor(props) {
@@ -27,6 +63,7 @@ class InputEx1 extends Component {
   handlePeriods = periods => {
     this.setState({ periods });
     this.handleBtnShow(periods.periodStart, periods.periodEnd);
+    this.setState({ turn: !this.state.turn });
   };
   btnShow = "yyyy-mm-dd";
   handleBtnShow = (periodStart, periodEnd) => {
@@ -35,15 +72,23 @@ class InputEx1 extends Component {
     } else if (periodStart == periodEnd) {
       this.btnShow = periodStart;
     } else {
-      this.btnShow = periodStart + " ~ " + periodEnd;
+      this.btnShow = periodStart + " - " + periodEnd;
     }
   };
 
   render() {
     return (
-      <div>
-        <input type="button" onClick={this.handleCalendarShow} value={this.btnShow} style={cssObject} />
-        {/* <input onClick={this.handleCalendarShow} value={this.state.periods.periodStart} /> */}
+      <div style={this.state.turn ? { width: "240px" } : { width: "150px" }}>
+        <InputEx1Div1>
+          <InputEx1Div2>
+            <InputEx1Date>Date</InputEx1Date>
+            <div>
+              <InputEx1Btn type="button" onClick={this.handleCalendarShow}>
+                <InputEx1BtnContent>{this.btnShow}</InputEx1BtnContent>
+              </InputEx1Btn>
+            </div>
+          </InputEx1Div2>
+        </InputEx1Div1>
         <div style={this.state.turn ? {} : { display: "none" }}>
           <Calendar callbackFunction={periods => this.handlePeriods(periods[0])} sizeOption="md" theme="light" />
         </div>
