d14f9df24816b1e0effa475ed67f536d7b0cc034
공정훈 <rhdtl78@gmail.com>
2019-02-16 21:29:03 +0900

    날짜 형식 변환 함수 formatDateString 등록

--- src/Containers/Provider/PropsProvider.js
+++ src/Containers/Provider/PropsProvider.js
@@ -28,8 +28,20 @@ class PropsProvider extends Component {
     }
   };
 
+  /**
+   * @function formatDateString - 날짜 형식 통일을 위한 변환 함수.
+   * @param {String} dateString - 날짜 형식을 변환할 스트링
+   * @return {String} - 변환된 날짜 스트링
+   */
+  formatDateString = dateString => {
+    const newDate = new Date(dateString);
+    return `${newDate.getFullYear()}-${newDate.getMonth() + 1}-${newDate.getDate()}`;
+  };
+
   actions = {
     addCalendarText: newDateObject => {
+      const { date } = newDateObject;
+      newDateObject.date = this.formatDateString(date);
       this.setState({
         objectSetText: [...this.state.objectSetText, newDateObject]
       });
