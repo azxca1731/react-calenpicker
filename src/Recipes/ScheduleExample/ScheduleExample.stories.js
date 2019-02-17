64e5f311a05ba679587a48cc203f28522f0c2a54
azxca1731 <azxca1731@gmail.com>
2019-02-17 20:49:41 +0900

    노트로 코드 추가

--- src/Recipes/ScheduleExample/ScheduleExample.stories.js
+++ src/Recipes/ScheduleExample/ScheduleExample.stories.js
@@ -3,5 +3,8 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 
 import ScheduleRecipeWithChart from "./ScheduleRecipeWithChart";
+import code from "./code.md";
 
-storiesOf("Recipes", module).add("캘린더 스케줄 예제 1", () => <ScheduleRecipeWithChart />);
+storiesOf("Recipes", module).add("캘린더 스케줄 예제 1", () => <ScheduleRecipeWithChart />, {
+  notes: { markdown: code }
+});
