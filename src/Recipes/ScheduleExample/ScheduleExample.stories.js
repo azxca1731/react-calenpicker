ffdb621faa08f9dd52fbbebabca0837066acc137
azxca1731 <azxca1731@gmail.com>
2019-02-17 17:39:19 +0900

    스케줄 예시 추가

---
+++ src/Recipes/ScheduleExample/ScheduleExample.stories.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+
+import ScheduleRecipeWithChart from "./ScheduleRecipeWithChart";
+
+storiesOf("Recipes", module).add("캘린더 스케줄 예제 1", () => <ScheduleRecipeWithChart />);
